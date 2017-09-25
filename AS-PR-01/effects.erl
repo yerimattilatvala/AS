85dfd54b2430af167e28c2530d157f83fcfd7aad
Yeray <yeraymendez07@gmail.com>
2017-09-25 19:40:26 +0200

    effects listo

--- AS-PR-01/effects.erl
+++ AS-PR-01/effects.erl
@@ -1,13 +1,17 @@
 -module(effects).
--export([print/1]).
+-export([print/1,even_print/1]).
 
 print(0) -> ok;
 print(N) when N > 0 ->
     io:format("~p, ",[N]),
     print(N-1).
-
-
-
+ 
+even_print(0)-> ok;
+even_print(N) when N rem 2 =:=1->
+    even_print(N-1);
+even_print(N) when N rem 2 =:=0 ->
+    io:format("~p, ",[N]),
+    even_print(N-1).
     
 
     
