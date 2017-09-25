48a253a311ef8e53b975d63daa4160fd2de14792
Yeray <yeraymendez07@gmail.com>
2017-09-26 00:01:31 +0200

    todo listo

--- AS-PR-01/manipulating.erl
+++ AS-PR-01/manipulating.erl
@@ -1,5 +1,6 @@
 -module(manipulating).
--export([reverse/1,filter/2,concatenate/1]).
+-export([reverse/1,filter/2,concatenate/1,flatten/1]).
+-export([concatenateaux/2]).
 
 reverse(L)-> reverse(L,[]).
 
@@ -10,4 +11,23 @@ reverse([H|T],R)->
 
 filter(L,N)->
     [X || X <-L, X =< N].
+
+concatenateaux(R,[])->
+    R;
+concatenateaux([],L)->
+    L;
+concatenateaux([H|T],L)->
+    [H|concatenateaux(T,L)].
+
+concatenate([])->
+    [];
+concatenate([H])->
+    H;
+concatenate([H|T])->
+    concatenateaux(H,concatenate(T)).
+
+flatten([[H|T1]|T2]) -> flatten([H,T1|T2]);
+flatten([[]|T]) -> flatten(T);
+flatten([E|T]) -> [E|flatten(T)];
+flatten([]) -> [].
 
