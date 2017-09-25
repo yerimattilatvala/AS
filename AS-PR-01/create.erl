55672b7832ed00ea39bc1296febfcd1232bebf6f
Yeray <yeraymendez07@gmail.com>
2017-09-25 11:29:30 +0200

    carpetas creadas y modulo create listo

---
+++ AS-PR-01/create.erl
@@ -0,0 +1,12 @@
+-module(create).
+-export([create/1,reverse_create/1]).
+
+create(0)->
+    [];
+create(N)->
+    create(N-1) ++ [N].
+
+reverse_create(0)->
+    [];
+reverse_create(N)->
+    [N] ++ reverse_create(N-1).
