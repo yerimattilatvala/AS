0181f94a1ca3dcdc802299667bc8a0c3dc51ff34
Yeray <yeraymendez07@gmail.com>
2017-10-09 15:52:17 +0200

    ultimas modificaciones del modulo echo(AS-PR-04 LISTA)

--- AS-PR-04/echo.erl
+++ AS-PR-04/echo.erl
@@ -2,19 +2,18 @@
 -export([start/0,stop/0,print/1,init/0]).
 
 start()->
-    register(echo, spawn(?MODULE,init,[])),
-    ok.
+    register(echo, spawn(?MODULE,init,[])).
 
 init()->
     loop().
 
 stop()->
-    echo ! {stop}.
+    echo ! stop.
 
 print(Msg)->
     echo ! {print,Msg,self()},
     receive
-        {echo_reply,Reply}->Reply
+        {echo_reply,_}->Msg
     end.
 
 loop()->
@@ -22,9 +21,10 @@ loop()->
         {print,M,From}->
             From ! {echo_reply,M},
             loop();
-        {stop}->
+        stop->
             case whereis(echo) of
-                    undefined -> ok; % No registered process with name echo
-                    Pid -> exit(Pid, normal),
+                    Pid -> 
+                        exit(Pid, normal),
+                        unregister(echo)
                     end
     end.
