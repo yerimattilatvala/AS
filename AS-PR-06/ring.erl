f67a92b881edf480649083b6ef85e2d880b11ea4
Yeray <yeraymendez07@gmail.com>
2017-10-25 00:25:53 +0200

    ring se recupera si muere cualquier proceso, y si queda uno termina

--- AS-PR-06/ring.erl
+++ AS-PR-06/ring.erl
@@ -6,9 +6,10 @@ start(NumProcs)->
 
 rootProcess(NumProcs)->
     Pid = spawn_link(ring,startProcess,[NumProcs,self()]),
-    io:format("Root Process ~w and child ~w ~n",[self(),Pid]),
     rootProcess(NumProcs-1,Pid,[Pid]).
 rootProcess(0,ChildPid,L)->
+    io:format("Root Process ~w talk to a Process ~w ~n",[self(),ChildPid]),
+    process_flag(trap_exit, true),
     rootLoop(L,ChildPid);
 rootProcess(N,ChildPid,L)->
     Pid = spawn_link(ring,startProcess,[N,ChildPid]),
@@ -25,38 +26,100 @@ numberProcess([])->
 numberProcess([_| T])->
     1 + numberProcess(T).
 
+
+index(H,[H | _])->
+    1;
+index(H,[_|T])->
+    1+index(H,T).
+
+item([H|_],0)->
+    H;
+item([_|T],N)->
+    item(T,N-1).
+
+
+first([])->
+    [];
+first([H|_])->
+    H.
+
 rootLoop(ProcessList,ChildPid)->
     receive
         {0,M}->
-                ChildPid ! {0,M},
-                rootLoop(ProcessList,ChildPid);
+            ChildPid ! {0,M},
+            rootLoop(ProcessList,ChildPid);
         {MsgNum,Message}->
             ChildPid ! {MsgNum,Message},
             rootLoop(ProcessList,ChildPid);
         {transmite_message, MsgNum, Message}->
-                Cicles = MsgNum * numberProcess(ProcessList),
-                ChildPid ! {Cicles,Message},
-                rootLoop(ProcessList,ChildPid);
+            Cicles = MsgNum * numberProcess(ProcessList),
+            ChildPid ! {Cicles,Message},
+            rootLoop(ProcessList,ChildPid);
+        {'EXIT',Pid,_}->
+            L = lists:last(ProcessList),
+            I = first(ProcessList),
+            if Pid == I ->
+                L2 = lists:delete(Pid,ProcessList),
+                unlink(Pid),
+                PidN = first(L2),
+                io:format("~w ~n",[PidN]),
+                if PidN == []->
+                    ok;
+                true-> 
+                    PidN ! {root,self()},
+                    rootLoop(L2,PidN)
+                end;
+            true->
+                if Pid == L ->
+                    L2 = lists:delete(Pid,ProcessList),
+                    unlink(Pid),
+                    PidN = lists:last(L2),
+                    if PidN == []->
+                        ok;
+                    true-> 
+                        PidN ! {root,self()},
+                        rootLoop(L2,ChildPid)
+                    end;
+                true->
+                    Pos = index(Pid,ProcessList),
+                    NextPid = item(ProcessList,Pos),
+                    ChildPid ! {change,Pos-1,NextPid},
+                    unlink(Pid),
+                    rootLoop(lists:delete(Pid,ProcessList),ChildPid)
+                end
+            end;
         stop->  
             ChildPid ! stop,
             unregister(root),
             io:format("Root Process finishing ~w ~n",[self()])
+        after 100->
+            N = numberProcess(ProcessList),
+            if N == 1 ->
+                io:format("Only have one process.END ~n"),
+                ok;
+            true->
+                rootLoop(ProcessList,ChildPid)
+            end
     end.
 
 startProcess(N,Pid)->
-    io:format("Slave Process ~w with PIP ~w and child ~w ~n",[N,self(),Pid]),
+    io:format("Slave Process ~w with PID ~w talk to a Process ~w ~n",[N,self(),Pid]),
     slaveLoop(Pid,N).
 
 slaveLoop(ChildPid,N)->
     receive
         stop->
             ChildPid ! stop,
-            io:format("Slave Process ~w  is finishing with PID ~w and child ~w ~n",[N,self(),ChildPid]);
+            io:format("Slave Process ~w  is finishing with PID ~w ~n",[N,self()]);
         {0,_}->
             io:format("Finish ~n"),
             slaveLoop(ChildPid,N);
+        {change,N,Pid}->
+            slaveLoop(Pid,N);
+        {root,From}->
+            slaveLoop(From,N);
         {MsgNum,Message}->
-            io:format("Slave Process ~w with PID ~w send ~w to child ~w ~n",[N,self(),Message,ChildPid]),
+            io:format("Slave Process ~w with PID ~w send ~w to  ~w ~n",[N,self(),Message,ChildPid]),
             ChildPid ! {MsgNum-1,Message},
             slaveLoop(ChildPid,N)
     end.
